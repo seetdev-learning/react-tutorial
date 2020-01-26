6c980e488b4c4e80cb82b91256a7415cdc9312f8
Darren Seet <darren@seet.dev>
2020-01-25 23:27:57 -0500

    feat: Adding multi player functionality

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = [...this.state.squares];
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "0";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
@@ -32,7 +36,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "0"}`;
 
     return (
       <div>
